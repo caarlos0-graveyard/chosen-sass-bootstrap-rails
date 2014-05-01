c0f9ec9a37b6198a39cc340f7200d4eaedc95f1c
David John <djohn@arch-no.org>
2014-05-01 09:13:32 -0500

    add precompile assets

--- lib/chosen-sass-bootstrap-rails.rb
+++ lib/chosen-sass-bootstrap-rails.rb
@@ -3,6 +3,9 @@ module Chosen
     module Bootstrap
       module Rails
         class Engine < ::Rails::Engine
+          config.assets.precompile += %w(
+            chosen_bootstrap.css chosen_bootstrap_variables.css chosen-sprite.png
+          )
         end
       end
     end
